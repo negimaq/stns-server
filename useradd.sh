a631b3406cadcef7189b5896c2ae3328a0894cfb
negimaq <negimaq@gmail.com>
2020-12-08 16:31:53 +0900

    Refactor info and error message

--- useradd.sh
+++ useradd.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+info_msg() {
+	echo -e "\e[32m[INFO]\e[m "$1 >&2
+}
+
+error() {
+	echo -e "\e[31m[ERROR]\e[m "$1 >&2
+	exit 1
+}
+
 # ユーザ名と公開鍵を入力
 if [ $# -eq 4 ]; then
 	user=$1
 	key="$2 $3 $4"
-	echo -e "\e[32m[INFO]\e[m ユーザ名: \e[1m"$user"\e[m" >&2
-	echo -e "\e[32m[INFO]\e[m 公開鍵: \e[1m"$key"\e[m" >&2
+	info_msg "ユーザ名: \e[1m"$user"\e[m"
+	info_msg "公開鍵: \e[1m"$key"\e[m"
 else
 	read -p "ユーザ名: " user
 	read -p "公開鍵: " key
@@ -13,13 +22,11 @@ fi
 
 # ユーザ名と公開鍵の形式チェック
 if ! `echo $user | grep -Eq "^[a-z_]([a-z0-9_-]{0,31}|[a-z0-9_-]{0,30}\$)$"`; then
-	echo -e "\e[31m[ERROR]\e[m 入力されたユーザ名が正しくありません: \e[1m"$user"\e[m" >&2
-	exit 1
+	error "入力されたユーザ名が正しくありません: \e[1m"$user"\e[m"
 fi
 echo $key | ssh-keygen -lf /dev/stdin &> /dev/null
 if [ $? -ne 0 ]; then
-	echo -e "\e[31m[ERROR]\e[m 入力された公開鍵が正しくありません: \e[1m"$key"\e[m" >&2
-	exit 1
+	error "入力された公開鍵が正しくありません: \e[1m"$key"\e[m"
 fi
 
 # ユーザ名の重複チェックと次のidを取得
@@ -29,8 +36,7 @@ for conf in $(find ./conf.d/ -type f); do
 	conf_id=`echo $conf | sed -r 's/^([0-9]{1,5})-.+\.conf$/\1/'`
 	conf_user=`echo $conf | sed -r 's/^[0-9]{1,5}-(.+)\.conf$/\1/'`
 	if [ $user = $conf_user ]; then
-		echo -e "\e[31m[ERROR]\e[m 入力されたユーザ名のconfファイルは既に存在します: \e[1m"$conf"\e[m" >&2
-		exit 1
+		error "入力されたユーザ名のconfファイルは既に存在します: \e[1m"$conf"\e[m"
 	fi
 	if [ $conf_id -gt $max_id ]; then
 		max_id=$conf_id
@@ -50,4 +56,4 @@ keys = ["${key}"]
 id = ${next_id}
 users = ["${user}"]
 EOS
-echo -e "\e[32m[INFO]\e[m confファイルを作成しました: \e[1m"./conf.d/${next_id}-${user}.conf"\e[m" >&2
+info_msg "confファイルを作成しました: \e[1m"./conf.d/${next_id}-${user}.conf"\e[m"
